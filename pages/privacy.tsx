30af7fc744e129b2e3e4fc205291c802c9d26790
Mufaqar Islam <mufaqar@gmail.com>
2022-12-06 17:47:46 +0500

    privacy

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -1,29 +1,38 @@
 
-import Header from "../components/header";
+// import Header from "../components/header";
 
-import { de, en } from '../public/translation'
-import { useRouter } from 'next/router'
-import NavList from '../components/navlist';
-import Footer from '../components/footer';
-import Privacy from '../components/Privacy';
+// import { de, en } from '../public/translation'
+// import { useRouter } from 'next/router'
+// import NavList from '../components/navlist';
+// import Footer from '../components/footer';
+// import Privacy from '../components/Privacy';
 
-export default function PrivacyPage() {
-	interface MyCodeParams {
-		about: (param: any) => void
-	}
+// export default function PrivacyPage() {
+// 	interface MyCodeParams {
+// 		about: (param: any) => void
+// 	}
 
-	const router = useRouter()
-	const { locale } = router;
-	const t = locale === "en" ? en : de
-	const { navlist, contact } = t;
+// 	const router = useRouter()
+// 	const { locale } = router;
+// 	const t = locale === "en" ? en : de
+// 	const { navlist, contact } = t;
 
+// 	return (
+// 		<>
+// 			{/* <NavList navlist={navlist[0]} /> */}
+// 			<div className="main">
+// 				<Privacy />
+// 			</div>
+// 			<Footer contact={contact[0]} />
+// 		</>
+// 	)
+// }
+
+
+import React from 'react'
+
+export default function Privacy() {
 	return (
-		<>
-			{/* <NavList navlist={navlist[0]} /> */}
-			<div className="main">
-				<Privacy />
-			</div>
-			<Footer contact={contact[0]} />
-		</>
+		<div>Privacy</div>
 	)
 }
